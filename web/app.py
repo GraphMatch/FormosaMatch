aa2f6b890fe6e59fc838d38a470e05aba2f4ac63
Victor Rolando Sanchez Jara <vrsj@sd202068.HUNG.ab.nthu.edu.tw>
2016-12-22 16:47:42 +0800

    Add postgresql container and user models (for sql) for authentication

--- web/app.py
+++ web/app.py
@@ -4,69 +4,67 @@
 """ application file """
 from flask import Flask
 from flask import request, redirect, session, abort, url_for, render_template
-# from models import queries, user, language
-from models import queries, user
+from flask.ext.sqlalchemy import SQLAlchemy
 from py2neo import Graph
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 graph = Graph('http://neo4j:neo4j@192.168.99.100:7474/db/data/')
+db = SQLAlchemy(app)
 
+from modelsneo import *
+from modelssql import *
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """ index handler """
     #users = queries.get_users(graph)
     return render_template('index.html')
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#         """ register handler """
-#     error = None
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         username = request.form['username']
-#         password = request.form['password']
-#         if len(email) < 1:
-#             error = 'You must give us an email.'
-#         elif len(password) < 5:
-#             error = 'Your password must be at least 5 characters.'
-#         elif not user.User(graph=graph, email=email, username=username).register(password):
-#             error = 'A user with that email already exists.'
-#         else:
-#             session['email'] = email
-#             session['username'] = username
-#             return redirect(url_for('index'))
-#
-#     # return render_template('register.html', error=error)
-#     return render_template('index.html', error=error)
-#
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     """ login handler """
-#     error = None
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         password = request.form['password']
-#         if not user.User(graph=graph, email=email).verify_password(password):
-#             if not user.User(graph=graph, username=email).verify_password(password):
-#                 error = 'Invalid login.'
-#         if error ==  None:
-#             _user = user.User(graph=graph, email=email).find()
-#             if _user == None:
-#                 _user = user.User(graph=graph, username=email).find()
-#             session['email'] = email
-#             session['username'] = _user['username']
-#             return redirect(url_for('index'))
-#     # return render_template('login.html', error=error)
-#     return render_template('index.html', error=error)
-#
-# @app.route('/logout')
-# def logout():
-#     """ logout handler """
-#     session.pop('email', None)
-#     session.pop('username', None)
-#     return redirect(url_for('index'))
+@app.route('/api/register', methods=['POST'])
+def register():
+    json_data = request.json
+    user = User(
+        email=json_data['email'],
+        password=json_data['password']
+    )
+    try:
+        db.session.add(user)
+        db.session.commit()
+        status = 'success'
+    except:
+        status = 'this user is already registered'
+    db.session.close()
+    return jsonify({'result': status})
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    json_data = request.json
+    user = User.query.filter_by(email=json_data['email']).first()
+    if user and bcrypt.check_password_hash(
+            user.password, json_data['password']):
+        session['logged_in'] = True
+        status = True
+    else:
+        status = False
+    return jsonify({'result': status})
+
+
+@app.route('/api/logout')
+def logout():
+    session.pop('logged_in', None)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/api/status')
+def status():
+    if session.get('logged_in'):
+        if session['logged_in']:
+            return jsonify({'status': True})
+    else:
+        return jsonify({'status': False})
+
 
 if __name__ == '__main__':
     app.run()
