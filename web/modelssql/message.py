1a8e691d84931f7b1386e246271c86eac37249cd
KOUTOU W. Odilon <sirjameskoutou@yahoo.fr>
2017-01-08 20:29:16 +0800

    Message model updated for chat

--- web/modelssql/message.py
+++ web/modelssql/message.py
@@ -13,10 +13,13 @@ class Message(db.Model):
     match_id = db.Column(db.Integer, db.ForeignKey('match.id'), nullable=False)
     match = db.relationship("Match", back_populates="messages")
     sender_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    user = db.relationship("User", foreign_keys=[sender_id])
+    receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    sender = db.relationship("User", foreign_keys=[sender_id])
+    receiver = db.relationship("User", foreign_keys=[receiver_id])
 
-    def __init__(self, text, match_id, sender_id):
+    def __init__(self, text, match_id, sender_id, receiver_id):
         self.text = text
         self.timestamp = datetime.datetime.now()
         self.match_id = match_id
         self.sender_id = sender_id
+        self.receiver_id = receiver_id
